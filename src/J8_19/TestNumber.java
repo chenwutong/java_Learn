661323b2011e95966da4b5ff31f2a79d84ea5182
chenwutong <ibxcme@gmail.com>
2020-08-19 22:04:08 +0800

    push
    Signed-off-by: chenwutong <ibxcme@gmail.com>

---
+++ src/J8_19/TestNumber.java
@@ -0,0 +1,10 @@
+package src.J8_19;
+
+public class TestNumber {
+    public static void main(String[] args) {
+        int i = 5 ;
+        Integer it = new Integer(i);
+        int i2 = it.intValue();
+        System.out.println(it instanceof Number);
+    }
+}
